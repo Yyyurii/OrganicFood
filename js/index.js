147fc7547808b33fad1d322aee7f6910d3766f25
Yurii <yurii.kryvko@gmail.com>
2021-05-04 22:56:28 +0300

    Updates

---
+++ js/index.js
@@ -0,0 +1,25 @@
+console.log('hello')
+
+const iconMenu = document.querySelector('.label_checkbox');
+
+if (iconMenu) {
+  const menuBody = document.querySelector('.burger__list');
+  
+  iconMenu.addEventListener('click', (e) => {
+    menuBody.classList.toggle('_active');
+  })
+}
+
+//webp
+function testWebP(callback) {
+  var webP = new Image();
+  webP.onload = webP.onerror = function() {
+    callback(webP.height == 2);
+  };
+  webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+}
+testWebP(function (support) {
+  if (support == true) {
+    document.querySelector('body').classList.add('webp')
+  }
+});
